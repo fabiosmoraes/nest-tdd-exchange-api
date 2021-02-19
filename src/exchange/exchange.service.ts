7f990ad2bdd0990860bc43f9ccae8ccf5137ccea
Fabio Moraes <fabio.santosmoraes@hotmail.com>
2021-02-19 16:58:55 -0300

    Exchange testing #1

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
 
 @Injectable()
-export class ExchangeService {}
+export class ExchangeService {
+  constructor(private readonly currenciesService: CurrenciesService) {}
+  async convertAmount({ from, to, amount }): Promise<any> {
+    if (!from || !to || !amount) {
+      throw new BadRequestException();
+    }
+
+    const currencyFrom = this.currenciesService.getCurrency(from);
+    const currencyTo = this.currenciesService.getCurrency(to);
+  }
+}
